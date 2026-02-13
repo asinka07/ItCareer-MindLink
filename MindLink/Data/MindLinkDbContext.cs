b975c9b6af184105f0296d541e1da225e4d3d55b
asya.dishlieva@klevret.com <asya.dishlieva@klevret.com>
2026-02-13 20:30:57 +0200

    Database

---
+++ MindLink/Data/MindLinkDbContext.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MindLink.Data.Models;
+using System.Reflection.Emit;
+
+namespace MindLink.Data;
+
+public class MindLinkDbContext : DbContext
+{
+    public MindLinkDbContext(DbContextOptions<MindLinkDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<User> Users => Set<User>();
+    public DbSet<Role> Roles => Set<Role>();
+    public DbSet<Record> Records => Set<Record>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        // Roles
+        modelBuilder.Entity<Role>(entity =>
+        {
+            entity.HasKey(r => r.Id);
+            entity.Property(r => r.Name)
+                  .HasColumnName("Role")
+                  .HasMaxLength(50)
+                  .IsRequired();
+        });
+
+        // Users
+        modelBuilder.Entity<User>(entity =>
+        {
+            entity.HasKey(u => u.UserCode);
+
+            entity.Property(u => u.UserCode)
+                  .HasColumnType("char(6)");
+
+            entity.Property(u => u.Username)
+                  .HasMaxLength(50)
+                  .IsRequired();
+
+            entity.Property(u => u.Password)
+                  .HasMaxLength(1000)
+                  .IsRequired();
+
+            entity.Property(u => u.Name)
+                  .HasMaxLength(100)
+                  .IsRequired();
+
+            entity.Property(u => u.Gender)
+                  .HasColumnType("char(1)");
+
+            entity.HasOne(u => u.Role)
+                  .WithMany(r => r.Users)
+                  .HasForeignKey(u => u.RoleId)
+                  .OnDelete(DeleteBehavior.Restrict);
+
+        });
+
+        modelBuilder.Entity<Record>(entity =>
+        {
+            entity.HasKey(r => r.Id);
+
+            entity.Property(r => r.UserCode)
+                  .HasColumnType("char(6)")
+                  .IsRequired();
+
+            entity.Property(r => r.RecordText)
+                  .HasColumnType("text")
+                  .IsRequired();
+
+
+            entity.HasOne(r => r.User)
+                  .WithMany(u => u.Records)
+                  .HasForeignKey(r => r.UserCode)
+                  .OnDelete(DeleteBehavior.Cascade);
+        });
+    }
+}
